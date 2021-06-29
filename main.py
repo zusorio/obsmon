f0ca9518d54d18ba3cc02db94913f0d058161997
Tobias Messner <tobias.d.messner@gmail.com>
2021-06-29 20:53:25 +0200

    Add more stats

--- main.py
+++ main.py
@@ -5,10 +5,25 @@ from os import getenv
 from obswebsocket.exceptions import ConnectionFailure
 from prometheus_client import Gauge, start_http_server
 
-frames_dropped_count = Gauge('dropped_frames', 'Total frames dropped')
 bitrate = Gauge('bitrate', 'Bitrate in kbits')
+fps = Gauge('fps', 'Current framerate')
+
 frames_dropped_percent = Gauge('frames_dropped_percent', 'Percent of frames dropped')
+total_stream_time = Gauge('total_stream_time', 'Total time (in seconds) since the stream started')
+
+num_total_frames = Gauge('num_total_frames', 'Total number of frames transmitted since the stream started')
+frames_dropped_count = Gauge('dropped_frames', 'Total frames dropped')
+
+render_total_frames = Gauge('render_total_frames', 'Number of frames rendered')
+render_missed_frames = Gauge('render_missed_frames', 'Number of frames missed due to rendering lag')
+
+output_total_frames = Gauge('output_total_frames', 'Number of frames outputted')
+output_skipped_frames = Gauge('output_skipped_frames', 'Number of frames skipped due to encoding lag')
+
+average_frame_time = Gauge('average_frame_time', 'Average frame time in milliseconds')
 cpu_usage_percent = Gauge('cpu_usage_percent', 'CPU usage percent')
+memory_usage = Gauge('memory_usage', 'Current RAM usage in megabytes')
+free_disk_space = Gauge('free_disk_space', 'Free recording disk space in megabytes')
 
 
 logging.basicConfig(level=logging.INFO)
@@ -21,18 +36,38 @@ password = getenv("OBS_PASSWORD", "")
 
 
 def reset_data(*_):
-    frames_dropped_count.set(0)
     bitrate.set(0)
     frames_dropped_percent.set(0)
+    total_stream_time.set(0)
+    num_total_frames.set(0)
+    frames_dropped_count.set(0)
+    fps.set(0)
+    render_total_frames.set(0)
+    render_missed_frames.set(0)
+    output_total_frames.set(0)
+    output_skipped_frames.set(0)
+    average_frame_time.set(0)
     cpu_usage_percent.set(0)
+    memory_usage.set(0)
+    free_disk_space.set(0)
 
 
 def on_stream_status(message: events.StreamStatus):
     if message.getStreaming():
-        frames_dropped_count.set(message.getNumDroppedFrames())
         bitrate.set(message.getKbitsPerSec())
         frames_dropped_percent.set(message.getStrain())
+        total_stream_time.set(message.getTotalStreamTime())
+        num_total_frames.set(message.getNumTotalFrames())
+        frames_dropped_count.set(message.getNumDroppedFrames())
+        fps.set(message.getFps())
+        render_total_frames.set(message.getRenderTotalFrames())
+        render_missed_frames.set(message.getRenderMissedFrames())
+        output_total_frames.set(message.getOutputTotalFrames())
+        output_skipped_frames.set(message.getOutputSkippedFrames())
+        average_frame_time.set(message.getAverageFrameTime())
         cpu_usage_percent.set(message.getCpuUsage())
+        memory_usage.set(message.getMemoryUsage())
+        free_disk_space.set(message.getFreeDiskSpace())
     else:
         reset_data()
 
